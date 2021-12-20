83e66ae50d2d26c776bd05d604f20843130a7b8b
rashid semenov <rashid.semenov@absa.co.za>
2021-12-20 10:08:45 +0100

    adding landing page

---
+++ src/vendor/bootstrap/js/script.js
@@ -0,0 +1,11 @@
+function copyKey(key) {
+    /* Get the text field */
+    var copyText = document.getElementById(key);
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+}
